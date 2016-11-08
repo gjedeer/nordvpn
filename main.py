411d7286bb24e3cd07e4b5dc77fd606aaaf37591
Andrzej Godziuk <andrzej@godziuk.pl>
2016-11-08 17:59:46 +0100

    use stderr

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def get_best_server():
         best_servers.sort(key=lambda x:x.probe_time)
 
     for server in best_servers:
-        print server.domain, server.probe_time
+        sys.stderr.write(server.domain, server.probe_time)
 
     return best_servers[0]
 
